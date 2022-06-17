9883c53dd5a04af304505bc23f9ad92a88373d66
Yurii Krasniuk <y.krasniuk.rt01@kpi.ua>
2022-06-17 16:27:49 +0300

    start

--- Array.h
+++ Array.h
@@ -1,9 +1,18 @@
-#pragma once
+// Array.h -- definition Array-class
+#ifndef ARRAY_H_
+#define ARRAY_H_
+
+#include <iostream>		// input-output array
 
 
 // TODO:  write your Array class  here 
 class Array {
+public:
+
+private:
 
 
 
 };
+
+#endif // !ARRAY_H_
