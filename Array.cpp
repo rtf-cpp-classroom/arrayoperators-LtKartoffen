e830de7d4d5c555c8bbe8048e0e86c99eea824b1
Yurii Krasniuk <y.krasniuk.rt01@kpi.ua>
2022-06-18 02:07:43 +0300

    added constructors and assignment operator

--- Array.cpp
+++ Array.cpp
@@ -1,5 +1,59 @@
+// Array.cpp -- realization of Array's methods
 #include "Array.h"
-
-
 // TODO:  write your Array  methods  here 
 
+// Consturctors, destructor and assignment operator
+//---------------------------------------------------//
+// Default constructor - initialization an object
+template <class Item>
+Array<Item>::Array()
+{
+	m_size = m_step = m_current_index = 0;
+	m_pointer = nullptr;
+}
+// Constructor of copy - using deep copy for dynamic memory allocation
+template<class Item>
+Array<Item>::Array(const Array& object)
+{
+	m_size = object.getSize();
+	m_current_index = object.getElemSize - 1;
+	if (m_current_index == 0)						// if Array empty
+		m_pointer = nullptr;
+	else
+		for (int i = 0; i < object.getElemSize(); i++)
+			m_pointer[i] = object.m_pointer[i];
+}
+// Assignment operator
+template <class Item>
+Array<Item>& Array<Item>::operator=(const Array<Item>& object)
+{
+	if (this == &object)							// if self-assignment
+		return *this;
+	m_size = object.getSize();
+	m_current_index = object.getElemSize - 1;
+	if (m_current_index == 0)						// if Array empty
+		m_pointer = nullptr;
+	else
+		for (int i = 0; i < object.getElemSize(); i++)
+			m_pointer[i] = object.m_pointer[i];
+	return *this;
+}
+// Destructor - free memory
+template<class Item>
+Array<Item>::~Array()
+{
+	if (m_pointer == nullptr)
+	else
+		delete[] m_pointer;
+}
+//---------------------------------------------------//
+// Overloaded operators
+//---------------------------------------------------//
+// Get value from Array by index
+template<class Item>
+Item Array<Item>::operator[](int index) const
+{
+	if (index <= m_current_index and index >= 0)
+		return m_pointer[i];
+	return m_pointer[0];
+}
--- Array.h
+++ Array.h
@@ -4,7 +4,7 @@
 #include <iostream>								// input-output array
 // TODO:  write your Array class  here 
 // we will use generics idea - template class
-template <class T>
+template <class Item>
 class Array 
 {
 public:
@@ -14,37 +14,38 @@ public:
 	Array(const Array& object);						// constructor of copy
 
 	// Array Methods
-	void showElements() const;						// out in console
+	void showElements() const;							// out in console
 
-
+	// Get some member value
 	int getSize() const { return m_size; }			
-	int getElemSize() const { return m_current_index; }
+	int getElemSize() const { return m_current_index +1; }
 
-	void expandArray(int size);						// expand size of Array
-	bool setValue(T val);							// add new element to Array
-	void setArray(T* pArr, int size);				// add default array to Array
+	void expandArray(int size);							// expand size of Array
+	bool setValue(Item val);							// add new element to Array
+	void setArray(Item* pArr, int size);				// add default array to Array
 
 	// Overloaded operators
+	Array& operator=(const Array& object);			// very important assignment operator
 
 	Array operator+(const Array& object) const;		// will return new object Array
-	void operator+(T value);						// add some constant to all elements
-	void operator-(T value);						// sub some constant to all elements
+	void operator+(Item value);						// add some constant to all elements
+	void operator-(Item value);						// sub some constant to all elements
 
 	bool operator==(const Array& object) const;		// return true if Arrays'll be equivalent
 	bool operator!=(const Array& object) const;		// return false if Arrays'll be equivalent
 
 	void operator++();								// post-increment operation to all elements
-	Array operator+=(T value) const;				
-	T operator[](int index) const;					// indexation
+	Array operator+=(Item value) const;
+	Item operator[](int index) const;				// indexation
 
 	// Friends functions
 	friend std::ostream& operator<<(const std::ostream& os, const Array& object);	// out in console
-	friend void operator+(T value, Array& object);			
+	friend void operator+(Item value, Array& object);
 private:
 	int m_size;										// maximum size of array(variable)
 	int m_current_index;							// current index to last e-ment of Array
 	int m_step;										// how much increase size of Array 
-	T* m_pointer;									// pointer to type
+	Item* m_pointer;								// pointer to type
 };
 
 #endif // !ARRAY_H_
